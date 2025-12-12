aedc0ae096a3d203dc5019d9d1a49b0e9c0db1ca
sharwinar <sharwin.a@blackbuck.com>
2025-12-12 12:33:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def pilot_markers():
 # Start server
 # -------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=5000)
+
